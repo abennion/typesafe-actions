24fd884121a675212e773ab1c6337c1a04e976c1
Alex Bennion <abennion@ezlinks.com>
2019-05-02 18:02:28 -0500

    yes, queried something from discogs

--- codesandbox/src/features/discogs/components/ArtistView.tsx
+++ codesandbox/src/features/discogs/components/ArtistView.tsx
@@ -1,10 +1,40 @@
+import { RootState } from 'typesafe-actions';
 import * as React from 'react';
-
+import { connect } from 'react-redux';
+import { searchArtistsAsync } from '../actions';
 import ArtistList from './ArtistList';
 
-export default () => (
-  <section>
-    <h3>Artists</h3>
-    <ArtistList />
-  </section>
-);
+const mapStateToProps = (state: RootState) => ({
+  isLoading: state.artists.artists.loading,
+});
+const dispatchProps = {
+  requestArtists: searchArtistsAsync.request,
+};
+
+type Props = ReturnType<typeof mapStateToProps> & typeof dispatchProps;
+
+type State = {};
+
+class ArtistView extends React.Component<Props, State> {
+  render() {
+    const { isLoading, requestArtists } = this.props;
+    return (
+      <section>
+        <h3>Artists</h3>
+        <button
+          onClick={() => requestArtists('nirv')}
+          disabled={isLoading}>
+          Search
+        </button>
+        <ArtistList />
+      </section>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  dispatchProps
+)(ArtistView);
+
+// we need to connect this....
--- codesandbox/src/features/discogs/epics.ts
+++ codesandbox/src/features/discogs/epics.ts
@@ -1,34 +1,33 @@
 import { Epic } from 'redux-observable';
+// import { Artist } from 'MyModels';
 // import { timer } from 'rxjs';
-import { filter, map, switchMap } from 'rxjs/operators';
+import {
+  filter,
+  map,
+  // switchMap,
+  mergeMap,
+  // takeUntil,
+  tap
+} from 'rxjs/operators';
 import { RootAction, RootState, Services, isActionOf } from 'typesafe-actions';
-// import { ajax } from 'rxjs/ajax';
+import { ajax } from 'rxjs/ajax';
 import { searchArtistsAsync } from './actions';
-// import { getArtists } from './selectors';
 
-// const getUrl = (query: string) => `https://example.com?q=${query}`;
+const getArgs = (query: string) => ({
+  url: `https://api.discogs.com/database/search?q=${query}`,
+  method: 'GET',
+  headers: {
+    Authorization: 'Discogs token=RZZkINnywTUvdjQlBnWxxyVScrcrrbTIDYrUQWZT',
+  },
+});
 
-// const searchUsers = action$ =>
-//   action$.pipe(
-//     ofType(SEARCHED_USERS),
-//     map(action => action.payload.query),
-//     filter(q => !!q),
-//     switchMap(q =>
-//       // debounce
-//       timer(800).pipe(
-//         takeUntil(action$.pipe(ofType(CLEARED_SEARCH_RESULTS))),
-//         mergeMap(() =>
-//           merge(
-//             of(replace(`?q=${q}`)),
-//             ajax.getJSON(`https://api.github.com/search/users?q=${q}`).pipe(
-//               map(res => res.items),
-//               map(receiveUsers)
-//             )
-//           )
-//         )
-//       )
-//     )
-//   )
+// curl "https://api.discogs.com/database/search?q=Madlib"
+// -H "Authorization: Discogs token=RZZkINnywTUvdjQlBnWxxyVScrcrrbTIDYrUQWZT"
+// {"pagination": {"per_page": 50, "items": 2029, "page": 1,
+// "urls": {"last": "https://api.discogs.com/database/search?q=Madlib&per_page=50&page=41",
+// "next": "https://api.discogs.com/database/search?q=Madlib&per_page=50&page=2"},
+// "pages": 41},
+// "results": [{"thumb": "https://img.discogs.com/ixlt5
 
 export const searchArtistsEpic: Epic<
   RootAction,
@@ -36,38 +35,25 @@ export const searchArtistsEpic: Epic<
   RootState,
   Services
 > = (action$) =>
-  action$.pipe(
-    filter(isActionOf(searchArtistsAsync.request)),
-    switchMap(_ =>
-      map(searchArtistsAsync.success)
+    action$.pipe(
+      tap(console.log),
+      filter(isActionOf(searchArtistsAsync.request)),
+      // map(action => action.payload),
+      filter(action => !!action.payload),
+      tap(action => console.log('action', action)),
+      mergeMap(action => ajax(getArgs(action.payload)).pipe(
+        // map((res: any) => searchArtistsAsync.success(res.results as Artists[])),
+        tap(res => console.log('res', res)),
+        map((res: any) =>
+          res.response.results.map((el: any) => ({ id: el.id, title: el.title }))
+        ),
+        map(artists => searchArtistsAsync.success(artists))
+        // map((res: any) =>
+        //   searchArtistsAsync.success(
+        //     res.results.map((el: any) => ({ id: el.id, title: el.title })))
+        //   )
+        // )
+        // catchError((err: Error) => searchArtistsAsync.failure(err))
       )
-  );
-
-// switchMap(() =>
-//   from(api.todos.loadSnapshot()).pipe(
-//     map(loadTodosAsync.success),
-//     catchError((message: string) => of(loadTodosAsync.failure(message)))
-//   )
-// )
-// switchMap(q =>
-//   // debounce
-//   timer(800).pipe(
-//     // what if it's not a success, takeUntil not?
-//     takeUntil(action$.pipe(isActionOf(searchArtistsAsync.success))),
-//     mergeMap(() =>
-//       ajax.getJSON(`https://api.github.com/search/users?q=${q}`).pipe(
-//         map(res => res.items),
-//         map(receiveUsers)
-//       )
-//     )
-//   )
-// )
-// switchMap(() =>
-//   // from(api.todos.loadSnapshot()).pipe(
-//   //   map(searchArtistsAsync.success),
-//   //   catchError((message: string) => of(searchArtistsAsync.failure(message)))
-//   // )
-//   ajax(getUrl('nirva')).pipe(
-
-//   )
-// )
+      )
+    );
